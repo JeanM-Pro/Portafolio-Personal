e9dd01e1cfbf96af0a675a50e55bcb55548405d9
Jean Centeno <jeancenteno54@gmail.com>
2022-03-05 12:24:03 -0500

    proyecto funcionando

--- src/Portafolio.jsx
+++ src/Portafolio.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { AppRouter } from "./router/AppRouter";
-// import fondo from "./video/fondo.mp4";
+import fondo from "./video/fondo.mp4";
 
 export const Portafolio = () => {
   return (
     <>
       <AppRouter />
-      {/* <video autoPlay muted loop className="video-fondo">
+      <video autoPlay muted loop className="video-fondo">
         <source src={fondo} type="video/mp4" />
-      </video> */}
+      </video>
       <div className="capa-fondo"></div>
     </>
   );
