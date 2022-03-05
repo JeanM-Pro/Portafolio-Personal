6cd9ddba618d23ecb521e771c52486ee025ab6ea
Jean Centeno <jeancenteno54@gmail.com>
2022-03-05 07:57:34 -0500

    se agrego el video desde la pagina

--- src/Portafolio.jsx
+++ src/Portafolio.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 import { AppRouter } from "./router/AppRouter";
-import videoFondo from "./exports/video";
+import fondo from "./video/fondo.mp4";
 
 export const Portafolio = () => {
   return (
     <>
       <AppRouter />
-      {/* <video autoPlay muted loop className="video-fondo">
-        <source
-          src="https://www.facebook.com/100074368123002/videos/1364521867323210/"
-          type="video/mp4"
-        />
-      </video> */}
-      <iframe
-        className="video-fondo"
-        src="https://www.youtube.com/embed/xKkRgR4AuBA?autoplay=1"
-        allowFullScreen
-      ></iframe>
+      <video autoPlay muted loop className="video-fondo">
+        <source src={fondo} type="video/mp4" />
+      </video>
       <div className="capa-fondo"></div>
     </>
   );
--- src/exports/video.js
+++
@@ -1,7 +0,0 @@
-const videoFondo = "https://youtu.be/xKkRgR4AuBA";
-
-export default videoFondo;
-
-{
-  /* <iframe width="727" height="409" src="https://www.youtube.com/embed/xKkRgR4AuBA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */
-}
