e60c9ddfd4db620fee1f497c1922284fe088a02b
Jean Centeno <jeancenteno54@gmail.com>
2022-03-05 07:14:54 -0500

    prueba

--- src/Portafolio.jsx
+++ src/Portafolio.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { AppRouter } from "./router/AppRouter";
-import fondo2 from "./video/fondo2.mp4";
+import videoFondo from "./exports/video";
 
 export const Portafolio = () => {
   return (
     <>
       <AppRouter />
-      <video autoPlay muted loop className="video-fondo">
-        <source src={fondo2} type="video/mp4" />
-      </video>
+      {/* <video autoPlay muted loop className="video-fondo">
+        <source
+          src="https://www.facebook.com/100074368123002/videos/1364521867323210/"
+          type="video/mp4"
+        />
+      </video> */}
+      <iframe
+        className="video-fondo"
+        src="https://www.youtube.com/embed/xKkRgR4AuBA?autoplay=1"
+        allowFullScreen
+      ></iframe>
       <div className="capa-fondo"></div>
     </>
   );
